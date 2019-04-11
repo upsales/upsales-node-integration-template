bf3d5be55741848d11c99837b49c92f6f351bb64
Robin Sjögren <rsjogren@Robins-MacBook-Pro.local>
2019-04-11 11:14:17 +0200

    new upsales promise mock

--- __mocks__/@upsales/node-upsales-promise.js
+++ __mocks__/@upsales/node-upsales-promise.js
@@ -1,44 +1,37 @@
 const ActualUpsales = require.requireActual('@upsales/node-upsales-promise');
 const Upsales = jest.genMockFromModule('@upsales/node-upsales-promise');
-const actualUpsales = new ActualUpsales({key: 'key'});
 
+const actualUpsales = new ActualUpsales({key: 'key'});
 const instance = {};
 
+const bindJestFn = (propName, prototype, customfield) => {
+	Object.getOwnPropertyNames(prototype).forEach(fnName => {
+		if (typeof prototype[fnName] !== 'function') return;
+		if (customfield) {
+			instance[propName].customfields[fnName] = jest.fn(() => Promise.resolve());
+		} else {
+			instance[propName][fnName] = jest.fn(() => Promise.resolve());
+		}
+	});
+};
+
 Object.getOwnPropertyNames(actualUpsales).forEach(propName => {
-	if(actualUpsales[propName] === null || typeof actualUpsales[propName] !== 'object' || !Object.getPrototypeOf(actualUpsales[propName])){
-		return;
-	}
+	if (
+		actualUpsales[propName] &&
+		typeof actualUpsales[propName] === 'object' &&
+		Object.getPrototypeOf(actualUpsales[propName])
+	) {
+		instance[propName] = {};
+		bindJestFn(propName, Object.getPrototypeOf(actualUpsales[propName]));
 
-	instance[propName] = {};
-
-	if(propName === 'methods'){
-		Object.keys(actualUpsales[propName]).forEach((fnName) => {
-			if(typeof actualUpsales[propName][fnName] !== 'function'){
-				return;
-			}
-
-			instance[propName][fnName] = jest.fn(function() {
-				arguments[Object.keys(arguments).length - 1](null);
-			});
-		});
-	}else{
-		var prototype = Object.getPrototypeOf(actualUpsales[propName]);
-		Object.getOwnPropertyNames(prototype).forEach(fnName => {
-			if(typeof prototype[fnName] !== 'function'){
-				return;
-			}
-
-			instance[propName][fnName] = jest.fn(function() {
-				arguments[Object.keys(arguments).length - 1](null);
-			});
-		});
+		if (actualUpsales[propName].customfields) {
+			instance[propName].customfields = {};
+			bindJestFn(propName, Object.getPrototypeOf(actualUpsales[propName].customfields), true);
+		}
 	}
 });
 
-Upsales.mockImplementation(() => {
-	return instance;
-});
-
+Upsales.mockImplementation(() => instance);
 Upsales._instance = instance;
 
 module.exports = Upsales;
